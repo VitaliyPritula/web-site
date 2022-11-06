916735207ef3cffb4a2d906007793248cd0c686b
VitaliyPritula <54078944+VitaliyPritula@users.noreply.github.com>
2022-11-06 18:40:36 +0200

    додов слайдер свайпер

--- js/script.js
+++ js/script.js
@@ -1,33 +1,42 @@
-$(document).ready(function() {
-  $('.header-burger').click(function(event) {
-      $('.header-burger, .menu-nav').toggleClass('active');
-      $('body').toggleClass('lock');
+$(document).ready(function () {
+  $(".header-burger").click(function (event) {
+    $(".header-burger, .menu-nav").toggleClass("active");
+    $("body").toggleClass("lock");
   });
 });
 
-
-$(document).ready(function(){
-	$('.slider').slick({
-		arrows:true,
-		dots:true,
-		slidesToShow:1,
-		autoplay:true,
-		speed:10000,
-		autoplaySpeed:800,
-		responsive:[
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow:2
-				}
-			},
-			{
-				breakpoint: 550,
-				settings: {
-					slidesToShow:1
-				}
-			}
-		]
-	});
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: true,
+    dots: true,
+    slidesToShow: 1,
+    autoplay: true,
+    speed: 10000,
+    autoplaySpeed: 800,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 });
 
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // autoplay: true,
+  // speed: 800,
+  // бесконечность слайда
+  loop: true,
+});
