94a2f337fdf4ee8695916c3d31fbd422d6bb618e
VitaliyPritula <54078944+VitaliyPritula@users.noreply.github.com>
2022-11-13 10:46:49 +0200

    реалізований пошук по сайту і змінив адаптивність для секціїї корисні порадии

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     $("body").toggleClass("lock");
   });
 });
+
 var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
@@ -14,3 +15,30 @@ var swiper = new Swiper(".mySwiper", {
   // бесконечность слайда
   loop: true,
 });
+
+
+
+//Функция теста email
+function emailTest(input) {
+  return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+}
+
+
+/*=============================================================================================================*/
+document.querySelector("#elastic").oninput = function () {
+  let val = this.value.trim();
+  let elasticItems = document.querySelectorAll(".card .new-info");
+  if (val != "") {
+    elasticItems.forEach(function (elem) {
+      if (elem.innerText.search(RegExp(val, "gi")) == -1) {
+        elem.classList.add("hide");
+      } else {
+        elem.classList.remove("hide");
+      }
+    });
+  } else {
+    elasticItems.forEach(function (elem) {
+      elem.classList.remove("hide");
+    });
+  }
+};
