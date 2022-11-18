f3b478ef89869c57ab5b326bb572946f4adbbb34
VitaliyPritula <54078944+VitaliyPritula@users.noreply.github.com>
2022-11-18 19:08:45 +0200

    переробив попап, щоб правильно працюював

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,17 @@
 // создание переменной
 let btns = document.querySelectorAll("*[data-modal-btn]");
+let backgroundForCart = document.querySelector('.backgroundForCart');
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
     let name = btns[i].getAttribute("data-modal-btn");
     // для роботи, коли модальних вікон больше ніж 1
     let modal = document.querySelector("[data-modal-window='" + name + "']");
-    modal.classList.toggle("_active");
+    modal.classList.toggle("active");
+    backgroundForCart.classList.toggle('active');
     let close = modal.querySelector(".close-modal");
     close.addEventListener("click", function (e) {
-      modal.classList.remove("_active");
+      modal.classList.remove("active");
+      backgroundForCart.classList.remove('active');
     });
   });
 }
